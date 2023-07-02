3337a77cb996f9a61b6ef786394859975430cce9
rtm713 <rtm713@gmail.com>
2023-07-01 19:37:45 -0500

    added server.js and basic routes

---
+++ routes/notes.js
@@ -0,0 +1,5 @@
+const app = require('express');
+
+//this is where the magic will happen
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.listen(PORT, () => console.log(`Server listening @ http://localhost:${PORT}`));
