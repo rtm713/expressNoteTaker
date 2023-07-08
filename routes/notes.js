af2d5bef7f68847da5a7ecfc3e6a70a3b854a83f
rtm713 <rtm713@gmail.com>
2023-07-08 17:37:11 -0500

    functionality is complete i think

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 var fs = require('fs');
-
+var uuid = require('../helpers/uuid');
 
 router.get("/", (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -13,12 +13,26 @@ router.post("/", (req, res) => {
 
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         var notes = JSON.parse(data);
-        var newNote = req.body;
+        var newNote = { title: req.body.title, text: req.body.text, id: uuid()};
         notes.push(newNote);
         fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
             if (err) {
                 console.error(err);
               };
+            res.json('post added')
+        });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        var notes = JSON.parse(data);
+        var newNotes = notes.filter(note => note.id !==req.params.id)
+        fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err) => {
+            if (err) {
+                console.error(err);
+              };
+              res.json('post deleted')
         });
     });
 });
