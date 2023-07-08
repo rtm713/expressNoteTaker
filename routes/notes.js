83c76bcc604ba841342f7c6121e8ac0714b8f646
rtm713 <rtm713@gmail.com>
2023-07-08 16:54:10 -0500

    got post route to work

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,26 @@
-const app = require('express').Router();
-const path = require('path');
+const router = require('express').Router();
 var fs = require('fs');
 
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         res.send(data);
-    })
+    });
 });
 
-module.exports = app;
+router.post("/", (req, res) => {
+    console.log(req.body);
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        var notes = JSON.parse(data);
+        var newNote = req.body;
+        notes.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            if (err) {
+                console.error(err);
+              };
+        });
+    });
+});
+
+module.exports = router;
